99e49d86c97328911dd7221f9c0c20a800aafe6d
Nicolas Vermi <n.vermi@duckma.com>
2021-01-08 10:12:59 +0100

    Initial Commit

---
+++ HydroBerry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HydroBerry
+//
+//  Created by Nicolas Vermi on 08/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HydroBerry/HydroBerryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydroBerryApp.swift
+//  HydroBerry
+//
+//  Created by Nicolas Vermi on 08/01/21.
+//
+
+import SwiftUI
+
+@main
+struct HydroBerryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
